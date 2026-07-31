510b3e80ac4449f3e3852a862fc7d8a8e3397482
hitaroramos <hitarogabriel.ramos@gmail.com>
2026-07-31 19:12:39 -0300

    Nova atividade

--- SistemaAcademico/Ipersistencia.java
+++
@@ -1,7 +0,0 @@
-public interface Ipersistencia {
-    boolean incluir(Estudante estudante);
-    boolean excluir(String matricula);
-    boolean pesquisar(String matricula);
-    boolean lista();
-    boolean editar(String matricula, Estudante novoEstudante);
-}
